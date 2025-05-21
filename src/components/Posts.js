f55b2a42880bfed71b3da8f595ec89b6ef1ffe96
alimanhal <alimanhal@gmail.com>
2025-05-21 15:46:08 +0530

    Delete api call

--- src/components/Posts.js
+++ src/components/Posts.js
@@ -1,13 +1,46 @@
 import React, { useEffect, useState } from 'react';
-import { getPosts} from '../services/postService';
+import { getPosts, deletePost} from '../services/postService';
 
 export default function Posts() {
     const [posts, setPosts] = useState([]);
 
     useEffect(() => {
         console.log('Load Posts');
+        getPosts()
+          .then((result) => {
+            setPosts(result.data)
+          })
+          .catch((err) => {
+            console.error(err);
+          });
     }, []);
-    return <></>;
+
+    const handleDelete = (id) => {
+
+       deletePost(id)
+          .then((result) => {
+            setPosts(posts.filter((post) => post.id !== id));
+          })
+          .catch((err) => {
+            console.error(err);
+          });
+    }
+    return (
+        <div>
+            <h1>Posts</h1>
+
+            <ul>
+                {posts.map((post) => (
+                    <li key={post.id}>
+                        <h2>{post.title}</h2>
+                        <p>{post.body}</p>
+
+                        <button onClick={() => handleDelete(post.id)}>Delete</button>   
+                    </li>
+                ))}
+            </ul>
+        </div>
+    );
 
 }
 
