a22b6e08aefaa85f12e3b856ea88c31ab7965ff7
alimanhal <alimanhal@gmail.com>
2025-05-23 13:46:39 +0530

    editing post

--- src/components/Posts.js
+++ src/components/Posts.js
@@ -1,9 +1,10 @@
 import React, { useEffect, useState } from 'react';
-import { getPosts, deletePost} from '../services/postService';
+import { getPosts, deletePost, updatePost} from '../services/postService';
 import PostForm from './postFrom';
 
 export default function Posts() {
     const [posts, setPosts] = useState([]);
+    const [editPost, setEditPost] = useState(null); 
 
     useEffect(() => {
         console.log('Load Posts');
@@ -26,17 +27,25 @@ export default function Posts() {
           .catch((err) => {
             console.error(err);
           });
+
+    }
+
+    const startEdit = (post) => {
+        setEditPost(post);
+        
+
     }
     return (
         <div>
             <h1>Posts</h1>
-             <PostForm posts={posts} setPosts={setPosts}/>
+             <PostForm posts={posts} setPosts={setPosts} editPost={editPost} setEditPost={setEditPost} />
             <ul>
                 {posts.map((post) => (
                     <li key={post.id}>
                         <h2>{post.title}</h2>
                         <p>{post.body}</p>
 
+                        <button onClick={() => startEdit(post)}>Edit</button> 
                         <button onClick={() => handleDelete(post.id)}>Delete</button>   
                     </li>
                 ))}
--- src/components/postFrom.js
+++ src/components/postFrom.js
@@ -1,11 +1,75 @@
-// import { useState } from "react";
-import React   from "react";
+ import { useEffect, useState } from "react";
+import { createPost,updatePost } from "../services/postService";
 
-export default function PostForm({posts, setPosts}) {
+export default function PostForm({
+  posts,
+  setPosts, 
+  editPost, 
+  setEditPost 
+}) {
     const [title, setTitle] = useState(""); 
     const [body, setBody] = useState(""); 
 
+    useEffect(() => {
+      if(editPost) {
+        setTitle(editPost.title);
+        setBody(editPost.body);
+      }else {
+        setTitle("");
+        setBody("");
+      }
+
+    },[editPost])
+
+    const handleSubmit =   (e) => {
+        e.preventDefault();
+        if(editPost){
+
+          editingPost();
+
+        } else {
+
+          addPost();
+
+        }
+        setTitle("");
+        setBody("");
+    }
+    
+    const editingPost = () => {
+
+      updatePost(editPost.id, {title, body})
+            .then((response) => {
+              setPosts(posts.map(post => (post.id === editPost.id? response.id : post))); 
+          })
+          .catch((err) => {
+            console.error(err);
+          });
+    }
+       const addPost =  () => {
+            createPost({title, body})
+            .then((response) => {
+              setPosts([...posts,response.data]); 
+          })
+          .catch((err) => {
+            console.error(err);
+          });
+        }
+
     return (
-        <h1> Create a post</h1>
+       <form onSubmit={handleSubmit}>
+        <div>
+            title
+        </div>
+        <input type="text" value={title} onChange={(e) => setTitle(e.target.value)}></input>
+        <div>
+         Body
+        </div>
+        <textarea value={body} onChange={(e) => setBody(e.target.value)}></textarea>
+        <div>
+             <button type="submit">{editPost? "Ediit Post" : "Add Post"}</button>
+        </div>
+       
+       </form>
     )
 }
